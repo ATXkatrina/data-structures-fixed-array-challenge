a3c155f8f498a6334c14032dd6e638bcbc42e08a
Katrina Tollin <ktollin@gmail.com>
2017-02-09 15:00:31 -0600

    add initialize, get, set methods

--- fixed_array.rb
+++ fixed_array.rb
@@ -1,3 +1,16 @@
 class FixedArray
+  # attr_reader :a
+
+  def initialize(size)
+    @a = Array.new(size)
+  end
+
+  def get(index)
+    return @a[index]
+  end
+
+  def set(index, element)
+    @a[index] = element
+  end
 
 end
