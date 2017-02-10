8264a8f6fc754b3a141376cb1bc5fe9a32a3c903
Katrina Tollin <ktollin@gmail.com>
2017-02-10 15:49:45 -0600

    add rspec tests for FixedArray class

--- fixed_array_spec.rb
+++ fixed_array_spec.rb
@@ -1,5 +1,31 @@
 require_relative 'fixed_array'
 
 describe FixedArray do
-  
+  let(:test) { FixedArray.new(3) }
+    # before do
+    #   test.set(0, 'word test')
+    # end
+
+  it "returns an object with an array of n length" do
+    expect(test.length).to eq(3)
+  end
+
+  describe ".set" do
+    it "saves the n index with the given value" do
+      expect(test.set(1, 'words')).to eq('words')
+      expect { test.set(5, 'will fail') }.to raise_error(OutofBoundsException)
+    end
+  end
+
+  describe ".get" do
+    it "retrieves the n index from the fixed array" do
+      # expect(test.get(0)).to be('word test')
+      expect(test.get(1)).to be_nil
+      expect { test.get(4) }.to raise_error(OutofBoundsException)
+    end
+  end
+
+  describe ".length" do
+  end
+
 end
