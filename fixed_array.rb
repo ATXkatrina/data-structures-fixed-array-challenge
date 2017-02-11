0022041cdc955f4b073d71eadf00587fcf9f82ce
Katrina Tollin <ktollin@gmail.com>
2017-02-11 14:07:10 -0600

    add inbounds? method

--- fixed_array.rb
+++ fixed_array.rb
@@ -1,21 +1,27 @@
 class FixedArray
 
+  attr_reader :a
+
   def initialize(size)
     @a = Array.new(size)
   end
 
   def get(index)
-    raise OutofBoundsException if index >= @a.length
+    raise OutofBoundsException if !inbounds?(index)
     return @a[index]
   end
 
   def set(index, element)
-    raise OutofBoundsException if index >= @a.length
+    raise OutofBoundsException if !inbounds?(index)
     @a[index] = element
   end
 
   def length
     @a.length
+  end
+
+  def inbounds?(index)
+    index <= @a.length
   end
 
 end
